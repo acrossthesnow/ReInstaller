c90519e07d66afd7ba27a165fc039e25a73c149a
Brandon Phillips <brandon@snoware.dev>
2023-04-21 19:34:45 -0400

    Resolved issue with Phase2Installer not loading. It was an issue with the formating of the execute line in the task creation in Installer.

--- Scripts/Installer.ps1
+++ Scripts/Installer.ps1
@@ -16,7 +16,7 @@ if ((Test-Admin) -eq $false)  {
 
 #Start-Transcript -Path "logs.txt"
 robocopy /E $PSScriptRoot\.. $env:TEMP\ReInstaller\
-cd $env:TEMP\ReInstaller\Scripts
+Set-Location -Path $env:TEMP\ReInstaller\Scripts
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 $env:ChocolateyInstall = Convert-Path "$((Get-Command choco).Path)\..\.."   
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
@@ -29,14 +29,18 @@ python -m pip install --upgrade pip
 refreshenv
 pip install git+https://github.com/rsalmei/alive-progress
 refreshenv
+
+##Remove remnant task
+schtasks /delete /tn ReInstaller /f
+
 ## Create the action
-$action = New-ScheduledTaskAction -Execute 'C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe' -Argument '-File "$env:TEMP\ReInstaller\Scripts\Phase2Installer.ps1"'
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-noexit -executionPolicy bypass  -File Phase2Installer.ps1' -WorkingDirectory $env:TEMP\ReInstaller\Scripts\
 
 ## Set to run as local system, No need to store Credentials!!!
-$principal = New-ScheduledTaskPrincipal -UserId $env:ComputerName\$env:UserName -LogonType Interactive -RunLevel Highest
+$principal = New-ScheduledTaskPrincipal -UserId "$env:ComputerName\\$env:UserName" -LogonType Interactive -RunLevel Highest
 
 ## set to run at startup could also do -AtLogOn for the trigger
-$trigger = New-ScheduledTaskTrigger -AtLogon -User "$env:ComputerName\$env:UserName"
+$trigger = New-ScheduledTaskTrigger -AtLogon -User "$env:ComputerName\\$env:UserName"
 $Settings = New-ScheduledTaskSettingsSet -Priority 4
 
 ## register it (save it) and it will show up in default folder of task scheduler.
@@ -48,9 +52,9 @@ $location = "$PSScriptRoot\"
 $location|export-clixml -path "$env:TEMP\ReInstaller\Scripts\ORIGINALPATH.xml"
 
 
-pause
+Read-Host -Prompt "Press to enter to restart computer and launch ReInstaller..."
 #$RunOnceKey = "HKCU:\Software\Microsoft\Windows\CurrentVersion\RunOnce"
-#set-itemproperty $RunOnceKey "ReInstaller" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -executionPolicy bypass -File $env:TEMP\ReInstaller\Scripts\Phase2Installer.ps1')
+#set-itemproperty $RunOnceKey "ReInstaller" ('C:\Windows\System32\WindowsPowerShell\v1.0\Powershell.exe -noexit -executionPolicy bypass -File $env:TEMP\ReInstaller\Scripts\Phase2Installer.ps1')
 # Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 # Install-Module PSWindowsUpdate -Force
 # Import-Module PSWindowsUpdate
--- Scripts/Phase2Installer.ps1
+++ Scripts/Phase2Installer.ps1
@@ -1,8 +1,25 @@
-Read-Host -Prompt "Press to start ReInstaller"
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+
+Read-Host -Prompt "Press to enter to start ReInstaller..."
 $location = import-clixml -path "$env:TEMP\ReInstaller\Scripts\ORIGINALPATH.xml"
-cd $env:TEMP\ReInstaller\Scripts\
+Set-Location -Path $env:TEMP\ReInstaller\Scripts\
 python ReInstaller.py -e
 robocopy /COPYALL /IM /IS /IT /E $env:TEMP\ReInstaller\ $location 
-cd $location
-Remove-Item -Path $env:TEMP\ReInstaller -Recurse -Force
+Set-Location -Path $location
+#Remove-Item -Path $env:TEMP\ReInstaller -Recurse -Force
 schtasks /delete /tn ReInstaller /f
